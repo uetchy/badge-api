0ae501adc9ee75f0dc9ee41162268ed257e6d88d
Yasuaki Uechi <y@uechi.io>
2022-04-21 13:56:43 +0900

    chore: update deps

--- api/appstore/rating-count/[country]/[id].ts
+++ api/appstore/rating-count/[country]/[id].ts
@@ -1,22 +1,37 @@
 import { makeBadge } from "../../../../lib/badge";
 import { html } from "../../../../lib/fetch";
 
+function makeErrorBadge(status: string) {
+  return {
+    label: "rating",
+    color: "gray",
+    status: status,
+  };
+}
+
 export default makeBadge(async ({ country, id }) => {
   const url = `https://apps.apple.com/${country}/app/${id}`;
-  const dom = await html(url);
-  const schema = JSON.parse(
-    dom.window.document.querySelector(
-      'script[name="schema:software-application"]'
-    )?.textContent ?? ""
-  );
+  const dom = await html(url, {
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15",
+    },
+  });
+
+  const schemaContent = dom.window.document.querySelector(
+    'script[name="schema:software-application"][type="application/ld+json"]'
+  )?.textContent;
+
+  if (!schemaContent) {
+    return makeErrorBadge("missing schema");
+  }
+
+  const schema = JSON.parse(schemaContent);
 
   if (!schema.aggregateRating) {
-    return {
-      label: "rating",
-      color: "gray",
-      status: "unavailable",
-    };
+    return makeErrorBadge("unavailable");
   }
+
   const reviewCount = schema.aggregateRating.reviewCount;
 
   return {
